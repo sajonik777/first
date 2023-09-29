7488af6732fb6410400104addc41f267b8798834
John Hill <143519258+sajonik777@users.noreply.github.com>
2023-09-29 13:41:28 +0300

    Fixed an error with 'utf-8' decoding, defined missing variables Xtrain, X_test, y_train, y_test, features, model, and mse, and assisted with feature selection and evaluation. Also added code to complete a line.

--- project_root/app/ML-utilization/data_preprocessing.py
+++ project_root/app/ML-utilization/data_preprocessing.py
@@ -1,8 +1,14 @@
 import pandas as pd
+import chardet
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error
 
-def load_data(filename):
-    # Load data from CSV file
-    data = pd.read_csv(filename)
+def load_data(file_path):
+    with open(file_path, 'rb') as file:
+        result = chardet.detect(file.read())
+    encoding = result['encoding']
+    data = pd.read_csv(file_path, encoding=encoding)
     return data
 
 def preprocess_data(data):
@@ -12,25 +18,47 @@ def preprocess_data(data):
     return data
 
 def select_features(data):
-    # Select features for modeling
-    ...
-
-    return features
+    features = data[['Raw capacity']]
+    target = data['Usable capacity']
+    return features, target
 
 def split_data(features, target):
-    # Split data into training and testing sets
-    ...
-
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
     return X_train, X_test, y_train, y_test
 
 def train_model(X_train, y_train):
-    # Train a machine learning model
-    ...
-
+    model = LinearRegression()
+    model.fit(X_train, y_train)
     return model
 
 def evaluate_model(model, X_test, y_test):
-    # Evaluate the trained model
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
+    return mse
+
+def preprocess_data(data):
+    # Perform data preprocessing steps
     ...
 
+    return data
+
+def select_features(data):
+    features = data[['Raw capacity']]
+    target = data['Usable capacity']
+    return features, target
+
+def split_data(features, target):
+    X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+    return X_train, X_test, y_train, y_test
+
+def train_model(X_train, y_train):
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    return model
+
+def evaluate_model(model, X_test, y_test):
+    predictions = model.predict(X_test)
+    mse = mean_squared_error(y_test, predictions)
     return mse
+
+...rest of the code...
